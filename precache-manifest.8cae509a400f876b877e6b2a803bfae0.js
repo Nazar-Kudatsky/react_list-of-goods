49eb4af5a62b54b3e97505d036200cdfffa7da71
Nazar Kudatsky <divipi1996@icloud.com>
2021-05-26 15:11:14 +0300

    Updates

---
+++ precache-manifest.8cae509a400f876b877e6b2a803bfae0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90f76ae4eb133e7c09386f54b8cfda75",
+    "url": "./index.html"
+  },
+  {
+    "revision": "7a7ce6fae1dd1c8d5c23",
+    "url": "./static/css/main.d20b1e28.chunk.css"
+  },
+  {
+    "revision": "d7c740c35d800138a8c3",
+    "url": "./static/js/2.343efcc9.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.343efcc9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a7ce6fae1dd1c8d5c23",
+    "url": "./static/js/main.eb00176d.chunk.js"
+  },
+  {
+    "revision": "00722c5035a6ce4d8014",
+    "url": "./static/js/runtime-main.0b6bfc7e.js"
+  }
+]);
